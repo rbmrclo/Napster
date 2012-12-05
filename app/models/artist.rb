fa39062a0cc9a9c850bdba0272f42b78bf6ccece
rbmrclo <rbmrclo@hotmail.com>
2012-12-05 19:25:21 +0800

    Some edit

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   attr_accessible :bio, :gender, :name
-  validates :name, :presence => true, :length => {:maximum => 20}
+  validates :name, :presence => true, :length => {:maximum => 30}
   validates_presence_of :bio
   
   has_many :albums, :dependent => :destroy
